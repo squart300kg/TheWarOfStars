6e83b4b332fd829ee1b59de8fff2b811a9c934f2
liam <liam@mobidoo.co.kr>
2021-08-19 23:51:20 +0900

    서버코드: N/A 클라이언트: 결제화면 그리기 완료

--- app/src/main/java/com/the/war/of/thewarofstars/ui/home/sub/pay/PayActivity.kt
+++ app/src/main/java/com/the/war/of/thewarofstars/ui/home/sub/pay/PayActivity.kt
@@ -1,8 +1,11 @@
 package com.the.war.of.thewarofstars.ui.home.sub.pay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.ScrollingMovementMethod
+import android.text.style.UnderlineSpan
 import android.util.Log
+import android.view.View
 import com.iamport.sdk.data.sdk.IamPortRequest
 import com.iamport.sdk.data.sdk.PG
 import com.iamport.sdk.data.sdk.PayMethod
@@ -11,15 +14,98 @@ import com.the.war.of.thewarofstars.BaseActivity
 import com.the.war.of.thewarofstars.R
 import com.the.war.of.thewarofstars.databinding.ActivityPayBinding
 
+
 class PayActivity: BaseActivity<ActivityPayBinding>(R.layout.activity_pay){
 
     private val TAG = "PayActivityLog"
+
+    private var isPersonalUsageTermsClicked = false
+    private var isPayServiceTermsClicked    = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Iamport.init(this)
 
+        // 피드백받을 이메일에 밑줄
+        initializeUnderLine()
 
+        // 초기 뷰 세팅
+        initializeView()
+
+        binding {
+
+            tvPayPhone.apply {
+                setOnClickListener {
+                    tvPayPhone.isSelected = true
+                    tvPayKakao.isSelected = false
+                    tvPaySamsung.isSelected = false
+                }
+            }
+
+            tvPayKakao.apply {
+                setOnClickListener {
+                    tvPayPhone.isSelected = false
+                    tvPayKakao.isSelected = true
+                    tvPaySamsung.isSelected = false
+                }
+            }
+
+            tvPaySamsung.apply {
+                setOnClickListener {
+                    tvPayPhone.isSelected = false
+                    tvPayKakao.isSelected = false
+                    tvPaySamsung.isSelected = true
+                }
+            }
+
+            layoutTermsPersonalUsageTitle.apply {
+                setOnClickListener {
+                    when (isPersonalUsageTermsClicked) {
+                        true -> {
+                            tvTermsPersonalUsageContent.visibility = View.GONE
+                            isPersonalUsageTermsClicked = false
+                        }
+                        false -> {
+                            tvTermsPersonalUsageContent.visibility = View.VISIBLE
+                            tvTermsPersonalUsageContent.movementMethod = ScrollingMovementMethod()
+                            isPersonalUsageTermsClicked = true
+                        }
+                    }
+
+                }
+            }
+            layoutTermsPayServiceTitle.apply {
+                setOnClickListener {
+                    when(isPayServiceTermsClicked) {
+                        true -> {
+                            tvTermsPayServiceContent.visibility = View.GONE
+                            isPayServiceTermsClicked = false
+                        }
+                        false -> {
+                            tvTermsPayServiceContent.visibility = View.VISIBLE
+                            tvTermsPayServiceContent.movementMethod = ScrollingMovementMethod()
+                            isPayServiceTermsClicked = true
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+    }
+
+    private fun initializeView() {
+        dataBinding.tvPayPhone.isSelected = true
+    }
+
+    private fun initializeUnderLine() {
+        val content = SpannableString(dataBinding.tvGuideFeedbackEmail.text.toString())
+        content.setSpan(UnderlineSpan(), 7, 24, 0)
+        dataBinding.tvGuideFeedbackEmail.text = content
     }
 
     fun pay() {
