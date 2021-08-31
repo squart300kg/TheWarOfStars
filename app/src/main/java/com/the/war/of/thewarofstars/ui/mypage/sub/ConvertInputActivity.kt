131005ea04832839981b47d13b82b5c84decfcbe
liam <liam@mobidoo.co.kr>
2021-09-01 00:02:16 +0900

    서버코드: N/A 클라이언트: 환전 포인트 입력 UI작업중

--- app/src/main/java/com/the/war/of/thewarofstars/ui/mypage/sub/ConvertInputActivity.kt
+++ app/src/main/java/com/the/war/of/thewarofstars/ui/mypage/sub/ConvertInputActivity.kt
@@ -1,17 +1,19 @@
 package com.the.war.of.thewarofstars.ui.mypage.sub
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
 import android.widget.TextView
 import com.the.war.of.thewarofstars.BaseActivity
 import com.the.war.of.thewarofstars.R
 import com.the.war.of.thewarofstars.databinding.ActivityConvertInputBinding
+import java.text.DecimalFormat
 
 class ConvertInputActivity: BaseActivity<ActivityConvertInputBinding>(R.layout.activity_convert_input) {
+
+    private val TAG = "ConvertInputActivityLog"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,10 +24,148 @@ class ConvertInputActivity: BaseActivity<ActivityConvertInputBinding>(R.layout.a
 
             tvNumberOne.apply {
                 setOnClickListener {
-                    etInput.text = etInput.text.toString() + "1"
+                    clickNumber(ONE)
                 }
             }
+
+            tvNumberTwo.apply {
+                setOnClickListener {
+                    clickNumber(TWO)
+                }
+            }
+
+            tvNumberThree.apply {
+                setOnClickListener {
+                    clickNumber(THREE)
+                }
+            }
+
+            tvNumberFour.apply {
+                setOnClickListener {
+                    clickNumber(FOUR)
+                }
+            }
+
+            tvNumberFive.apply {
+                setOnClickListener {
+                    clickNumber(FIVE)
+                }
+            }
+
+            tvNumberSix.apply {
+                setOnClickListener {
+                    clickNumber(SIX)
+                }
+            }
+
+            tvNumberSeven.apply {
+                setOnClickListener {
+                    clickNumber(SEVEN)
+                }
+            }
+
+            tvNumberEight.apply {
+                setOnClickListener {
+                    clickNumber(EIGHT)
+                }
+            }
+
+            tvNumberNine.apply {
+                setOnClickListener {
+                    clickNumber(NINE)
+                }
+            }
+
+            tvNumberZero.apply {
+                setOnClickListener {
+                    clickNumber(ZERO)
+                }
+            }
+
+            tvNumberDoubleZero.apply {
+                setOnClickListener {
+                    clickNumber(DOUBLE_ZERO)
+                }
+            }
+
+            tvNumberErase.apply {
+                setOnClickListener {
+                    clickNumber(ERAISE)
+                }
+            }
+
+            tvAutoAccountAll.apply {
+                setOnClickListener {
+                    clickNumber(ALL)
+                }
+            }
+
+            tvAutoAccount100.apply {
+                setOnClickListener {
+                    clickNumber(MILLION)
+                }
+            }
+
+            tvAutoAccount10.apply {
+                setOnClickListener {
+                    clickNumber(TEN_THOUSAND)
+                }
+            }
+
+            tvAutoAccount5.apply {
+                setOnClickListener {
+                    clickNumber(FIVE_THOUSAND)
+                }
+            }
+
+            tvAutoAccount1.apply {
+                setOnClickListener {
+                    clickNumber(ONE_THOUSAND)
+                }
+            }
+
+
         }
+
+    }
+
+    private fun clickNumber(number: String) {
+        if (dataBinding.etInput.text.toString().length >= MAX_LENGTH && number != ERAISE)
+            return
+
+        when (number) {
+            ERAISE -> {
+                if (dataBinding.etInput.text.toString().length <= 1) {
+                    dataBinding.etInput.text = ""
+                    return
+                }
+
+                val formatter        = DecimalFormat("###,###")
+                val input            = dataBinding.etInput.text.toString()
+                val inputEraiseComma = (input).replace(",", "")
+                val inputInt         = inputEraiseComma.substring(0, inputEraiseComma.length - 1).toInt()
+                val result           = formatter.format(inputInt)
+
+                dataBinding.etInput.text = result
+            }
+            else -> {
+                val formatter        = DecimalFormat("###,###")
+                val input            = dataBinding.etInput.text.toString()
+                val inputEraiseComma = (input + number).replace(",", "")
+                val inputInt         = inputEraiseComma.toInt()
+                var result           = formatter.format(inputInt)
+
+                if (result.length >= MAX_LENGTH) {
+                    val resultEraiseComma = result.replace(",", "")
+                    val resultString      = resultEraiseComma.substring(0, 8).toInt()
+                    result = formatter.format(resultString)
+                }
+
+                dataBinding.etInput.text = result
+            }
+        }
+
+
 
     }
 
@@ -56,5 +196,28 @@ class ConvertInputActivity: BaseActivity<ActivityConvertInputBinding>(R.layout.a
 //        dataBinding.btnLogin.isSelected = email.isNotEmpty()
 //                && password.isNotEmpty()
 //                && password.length >= 8
+    }
+
+    companion object {
+        const val ERAISE      = "-1"
+        const val ONE         = "1"
+        const val TWO         = "2"
+        const val THREE       = "3"
+        const val FOUR        = "4"
+        const val FIVE        = "5"
+        const val SIX         = "6"
+        const val SEVEN       = "7"
+        const val EIGHT       = "8"
+        const val NINE        = "9"
+        const val ZERO        = "0"
+        const val DOUBLE_ZERO = "00"
+
+        const val ALL = "500000"
+        const val MILLION = "1000000"
+        const val TEN_THOUSAND = "100000"
+        const val FIVE_THOUSAND = "50000"
+        const val ONE_THOUSAND = "10000"
+
+        const val MAX_LENGTH = 10
     }
 }
